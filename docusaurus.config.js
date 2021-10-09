a977f662461bf5d62cdb4fcdf41bd96ff6632833
Jean-Louis Voiseux <jean-louis@altheria-solutions.com>
2021-10-09 21:30:02 +0100

    Updating CNAME

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   title: 'Altheria Documentation',
   tagline: 'The go-to place for the documentation of all Altheria products',
   url: 'https://altheriaorg.github.io',
-  baseUrl: '/altheria-docs/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
